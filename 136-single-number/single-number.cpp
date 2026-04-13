7e44e320d26cf2b5fe0cf09d8150b8b5204bbc2c
saksham0428 <166752220+saksham0428@users.noreply.github.com>
2026-04-13 23:59:21 +0530

    Time: 22 ms (5.67%) | Memory: 24.6 MB (7.69%) - LeetSync

---
+++ 136-single-number/single-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        map<int,int> m;
+        int ans=0;
+        for(int i:nums){
+            m[i]++;
+        }
+        for(auto i:m){
+            if(i.second==1){
+                ans=i.first;
+            }
+        }
+        return ans;
+    }
+};
